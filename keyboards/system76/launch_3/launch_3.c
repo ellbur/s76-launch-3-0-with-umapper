a8f1b71d48613db5461722c5744ca4db2e67ef1a
Owen Healy <owen@owenehealy.com>
2025-12-21 21:10:59 -0500

    Enable debugging

--- keyboards/system76/launch_3/launch_3.c
+++ keyboards/system76/launch_3/launch_3.c
@@ -141,6 +141,9 @@ void matrix_init_kb(void) {
 
 void keyboard_post_init_user(void) {
     system76_ec_rgb_layer(layer_state);
+    debug_enable   = true;
+    debug_matrix   = true;
+    debug_keyboard = false;
 }
 
 void matrix_scan_kb(void) {
@@ -235,10 +238,3 @@ layer_state_t layer_state_set_kb(layer_state_t layer_state) {
     return layer_state_set_user(layer_state);
 }
 
-#ifdef CONSOLE_ENABLE
-void keyboard_post_init_user(void) {
-    debug_enable   = true;
-    debug_matrix   = false;
-    debug_keyboard = false;
-}
-#endif  // CONSOLE_ENABLE
--- keyboards/system76/launch_3/rules.mk
+++ keyboards/system76/launch_3/rules.mk
@@ -3,7 +3,7 @@
 BOOTMAGIC_ENABLE = no       # Bootmagic Lite
 MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and system control
-CONSOLE_ENABLE = no         # Console for debug
+CONSOLE_ENABLE = yes        # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 DYNAMIC_KEYMAP_ENABLE = yes # Reconfigurable keyboard without flashing firmware
 EEPROM_DRIVER = wear_leveling
