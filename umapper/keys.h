a68c521eb4f65f63f3ab03b4ea08c1c46107b4cc
Owen Healy <owen@owenehealy.com>
2026-01-23 19:17:22 -0500

    Add umapper, though it has problems

---
+++ umapper/keys.h
@@ -0,0 +1,19 @@
+
+#pragma once
+
+#include <inttypes.h>
+#include <stdbool.h>
+
+#include "../tmk_core/common/keycode.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define MAX_KEYS 0xE8
+typedef enum hid_keyboard_keypad_usage key_code;
+
+#ifdef __cplusplus
+}
+#endif
+
---
+++ umapper/main.h
@@ -0,0 +1,8 @@
+
+#pragma once
+
+#include "../tmk_core/common/keyboard.h"
+
+void umapper_init(void);
+void umapper_action_exec(keyevent_t event);
+
